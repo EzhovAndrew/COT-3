0f6d7d2fc9682b6006c194ccbb9a3c8e299579b6
EzhovAndrew <ezhovandr@gmail.com>
2024-12-21 21:11:16 +0300

    README + logs

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import os
 import boto3
+from time import sleep
 
 
 def main():
-    # Create an S3 client
     s3 = boto3.client(
         's3',
         endpoint_url="http://minio:9000",
@@ -11,7 +11,8 @@ def main():
         aws_secret_access_key=os.getenv("CLIENT_PASSWORD")
     )
     for i in range(10000):
-        # Upload a file to S3
+        if i == 1200:
+            sleep(100)
         s3.upload_file('test.jpeg', os.getenv("BUCKET_NAME"), f'test-{i}.jpeg')
 
 if __name__ == "__main__":
